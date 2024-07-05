19533f917391a6fa057d28796db4ae3d2ec6710d
PaulSoia <p-shadow@ukr.net>
2024-07-05 17:36:23 +0300

    add navigation;

--- app/src/main/java/com/android/devtools/presentation/ui/screens/apicheck/ApiCheckScreen.kt
+++ app/src/main/java/com/android/devtools/presentation/ui/screens/apicheck/ApiCheckScreen.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
